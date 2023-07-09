60298a4433864aa334a1a226b0613f2286e944d5
Tong Tang <pichip22@hotmail.com>
2023-07-09 03:29:40 -0400

    partial complete at work

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,8 +1,13 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(arr):
+    call = []
+    for i, j in enumerate(arr):
+        call.append(f"{i + 1}. {j}")
+    return print('\n'.join(call))
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(arr):
+    summon = []
+    for i in arr:
+        summon.append(f"{i}!")
 
 def long_planeteer_calls():
     pass
